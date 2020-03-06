37e19e6a283971c707ac7a6aeedb2b7e494ca3cf
Todd Spainhour <toddspainhour@gmail.com>
2020-03-06 12:19:05 -0600

    button click events connected

--- src/javascripts/components/foodForm.js
+++ src/javascripts/components/foodForm.js
@@ -6,11 +6,17 @@ const printEatSection = () => {
   let domString = '';
 
   domString += `<h2>Fullness Score: ${getTamagotchiStats.full} </h2>`;
-  domString += '<button>Eat Healthy Food</button>';
-  domString += '<button>Eat Unhealthy Food</button>';
+  domString += '<button id="healthy-food-button">Eat Healthy Food</button>';
+  domString += '<button id="unhealthy-food-button">Eat Unhealthy Food</button>';
   // use single quotes unless it contains a templete literal. Then use the usual ` before and after.
 
   printToDom('eat', domString);
+
+  const eatHealthyFoodButtonEvent = () => console.error('this is inside your eatHealthyFoodButtonEvent');
+  const eatUnhealthyFoodButtonEvent = () => console.error('this is inside your eatUnhealthyFoodButtonEvent');
+
+  $('#healthy-food-button').click(eatHealthyFoodButtonEvent);
+  $('#unhealthy-food-button').click(eatUnhealthyFoodButtonEvent);
 };
 
 
